7f3849eabeffa3ec0e999c86605cf7176edd4be0
Sevada Ghazaryan <techsupport@spaecipher.com>
2023-07-13 13:07:28 +0200

    Configure DB connection cred's

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
 <?php
 // DB configs
-
+define('DB_HOST', 'local');
+define('DB_NAME', 'db_name');
+define('DB_USERNAME', 'db_user_name');
+define('DB_PASSWORD', 'db_user_pass');
 
 // Esteblish DB connection
+try {
+    $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('DB connection faild: ' . $e->getMessage());
+}
